3e6f2219515ac48eeb4b19772943053cab20844b
Alangumerov <a_gumerov@kbtu.kz>
2025-04-05 11:59:10 +0600

    Update racing1.py

--- racing1.py
+++ racing1.py
@@ -2,31 +2,31 @@ import pygame
 import random
 
 pygame.init()
-
+#дисплей
 WIDTH, HEIGHT = 400, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Гонщик")
-
+#скорость игрока и врагов
 player_speed = 4
 enemy_speed = 3
-
+#фон
 bg_image = pygame.image.load('images/AnimatedStreet.png')
 bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
-
+#картинки 
 player = pygame.image.load('images/Player.png')
 enemy = pygame.image.load('images/Enemy.png')
 coin=pygame.image.load('images/images.png')
 coin=pygame.transform.scale(coin, (30,30))
-
+#начальные координаты игрока
 player_x = 190
 player_y = 400
-
+#появление врагов
 enemy_x = random.randint(50, WIDTH - 50)
 enemy_y = -100
 
 moving_left = False
 moving_right = False
-
+#шрифт 
 font = pygame.font.Font(None, 50)
 game_over_text = font.render("GAME OVER", True, (255, 255, 255))
 
@@ -40,6 +40,7 @@ coin_score=0
 coin_score_font=pygame.font.Font(None,50)
 running = True
 while running:
+    #управление
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -59,7 +60,7 @@ while running:
             player_x -= player_speed
         if moving_right and player_x < WIDTH - player.get_width():
             player_x += player_speed
-
+        #появвление врагов и счет очков
         enemy_y += enemy_speed
         if enemy_y > HEIGHT:
             enemy_x = random.randint(50, WIDTH - 50)
@@ -74,7 +75,7 @@ while running:
         player_rect = pygame.Rect(player_x, player_y, player.get_width(), player.get_height())
         enemy_rect = pygame.Rect(enemy_x, enemy_y, enemy.get_width(), enemy.get_height())
         coin_rect=pygame.Rect(coin_x,coin_y, coin.get_width(), coin.get_height())
-
+         #столкновение
         if player_rect.colliderect(enemy_rect):
             game_over_flag = True
 
@@ -92,7 +93,7 @@ while running:
     screen.blit(coin,(coin_x,coin_y))
     screen.blit(coin_score_text,(300,10))
 
-
+  
     if game_over_flag:
         screen.fill((255, 0, 0))
         screen.blit(game_over_text, (100, 250))
